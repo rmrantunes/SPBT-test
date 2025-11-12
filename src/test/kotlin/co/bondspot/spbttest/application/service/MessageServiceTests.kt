68b8a143318c1e8b24c7a24357746f7993cd15cc
Rafael Antunes <rmrantunes.dev@gmail.com>
2025-11-12 11:42:06 -0400

    feat: add more tests to MessageService

--- src/test/kotlin/co/bondspot/spbttest/application/service/MessageServiceTests.kt
+++ src/test/kotlin/co/bondspot/spbttest/application/service/MessageServiceTests.kt
@@ -5,6 +5,7 @@ import co.bondspot.spbttest.domain.repository.MessageRepository
 import io.mockk.every
 import io.mockk.mockk
 import org.assertj.core.api.Assertions.assertThat
+import org.springframework.data.repository.findByIdOrNull
 import kotlin.test.Test
 
 
@@ -21,5 +22,30 @@ class MessageServiceTests {
 
         assertThat(result).isEqualTo(created)
 
+    }
+
+    @Test
+    fun `should return message by id`() {
+        val repository = mockk<MessageRepository>()
+        val message = Message("Text", "Some ID")
+        every { repository.findByIdOrNull(message.id!!) } returns message
+        val service = MessageService(repository)
+
+        val result = service.findMessageById(message.id!!)
+
+        assertThat(result).isEqualTo(message)
+
+    }
+
+    @Test
+    fun `should list messages`() {
+        val repository = mockk<MessageRepository>()
+        val list = listOf(Message("Text", "Some ID"), Message("Text2", "Some ID2"))
+        every { repository.findAll() } returns list
+        val service = MessageService(repository)
+
+        val result = service.findMessages()
+
+        assertThat(result).isEqualTo(list)
     }
 }
