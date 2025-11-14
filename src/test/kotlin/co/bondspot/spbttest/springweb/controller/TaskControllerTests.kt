4d0dd7a91da1693faa82371e2bd3b3ed8da53590
Rafael Antunes <rmrantunes.dev@gmail.com>
2025-11-14 11:00:45 -0400

    test: group task controller tests

--- src/test/kotlin/co/bondspot/spbttest/springweb/controller/TaskControllerTests.kt
+++ src/test/kotlin/co/bondspot/spbttest/springweb/controller/TaskControllerTests.kt
@@ -5,6 +5,8 @@ import co.bondspot.spbttest.springweb.service.TaskService
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.ninjasquad.springmockk.MockkBean
 import io.mockk.every
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
@@ -15,6 +17,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 
 @WebMvcTest(TaskController::class)
+@DisplayName("task controller")
 class TaskControllerTests(@param:Autowired private val objectMapper: ObjectMapper) {
 
     @Autowired
@@ -23,32 +26,39 @@ class TaskControllerTests(@param:Autowired private val objectMapper: ObjectMappe
     @MockkBean
     private lateinit var taskService: TaskService
 
-    @Test
-    fun `should create and return a task`() {
-        val body = CreateTaskReqDto("Uma tarefa qualquer")
-        val createdTask = body.toDomainEntity().copy(id = "some_id")
-        every { taskService.create(any()) } returns createdTask
+    @Nested
+    @DisplayName("when creating a task...")
+    inner class CreateTask() {
 
-        mockMvc.perform(
-            post("/task")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(body))
-        )
-            .andDo { print() }
-            .andExpect(status().isCreated)
-            .andExpect(header().string("Location", "/task/${createdTask.id}"))
-            .andExpect(jsonPath("$.id").value(createdTask.id))
-            .andExpect(jsonPath("$.title").value(createdTask.title))
+        @Test
+        fun `return it if input is valid`() {
+            val body = CreateTaskReqDto("Uma tarefa qualquer")
+            val createdTask = body.toDomainEntity().copy(id = "some_id")
+            every { taskService.create(any()) } returns createdTask
+
+            mockMvc.perform(
+                post("/task")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(body))
+            )
+                .andDo { print() }
+                .andExpect(status().isCreated)
+                .andExpect(header().string("Location", "/task/${createdTask.id}"))
+                .andExpect(jsonPath("$.id").value(createdTask.id))
+                .andExpect(jsonPath("$.title").value(createdTask.title))
+        }
+
+        @Test
+        fun `validate input if fields are missing or have type mismatch`() {
+            mockMvc.perform(
+                post("/task")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content("""{ "title": "" }""")
+            )
+                .andDo { print() }
+                .andExpect(status().isBadRequest)
+                .andExpect(jsonPath("$.errors[0]").value("'title' must not be blank"))
+        }
     }
 
-    @Test
-    fun `validate task input`() {
-        mockMvc.perform(
-            post("/task")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content("""{ "title": "" }""")
-        )
-            .andExpect(status().isBadRequest)
-            .andExpect(jsonPath("$.errors[0]").value("'title' must not be blank"))
-    }
 }
