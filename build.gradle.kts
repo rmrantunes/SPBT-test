6c749f4a392d8ae3d55bc72585db26fe2119291f
Rafael Antunes <rmrantunes.dev@gmail.com>
2025-11-14 10:13:27 -0400

    chore: initial add spring validation

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.flywaydb:flyway-core")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
---
+++ src/main/kotlin/co/bondspot/spbttest/springweb/configuration/GlobalExceptionHandler.kt
@@ -0,0 +1,28 @@
+package co.bondspot.spbttest.springweb.configuration
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.validation.FieldError
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+data class ResponseDto(val errors: List<String> = emptyList())
+
+@RestControllerAdvice
+class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun handleValidationExceptions(ex: MethodArgumentNotValidException): ResponseEntity<ResponseDto> {
+        val errors = buildList<String> {
+            ex.bindingResult.allErrors.forEach { error ->
+                if (error is FieldError) add("'${error.field}' ${error.defaultMessage ?: "invalid field"}")
+                else error.defaultMessage?.let { add(it) }
+
+            }
+        }
+
+        println(ResponseEntity(ResponseDto(errors = errors), HttpStatus.BAD_REQUEST))
+
+        return ResponseEntity.badRequest().body(ResponseDto(errors = errors))
+    }
+}
