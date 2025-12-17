529eafe43f68ec30cccfccd795b6f77290e28e52
Rafael Antunes <rmrantunes.dev@gmail.com>
2025-12-17 16:36:23 -0400

    feat: implement handleTaskNewEvent

--- src/main/kotlin/co/bondspot/spbttest/application/service/TaskEventsService.kt
+++ src/main/kotlin/co/bondspot/spbttest/application/service/TaskEventsService.kt
@@ -1,10 +1,27 @@
 package co.bondspot.spbttest.application.service
 
+import co.bondspot.spbttest.domain.contract.IFgaProvider
+import co.bondspot.spbttest.domain.contract.IFullTextSearchProvider
 import co.bondspot.spbttest.domain.contract.ITaskEventsService
+import co.bondspot.spbttest.domain.entity.Account
+import co.bondspot.spbttest.domain.entity.FgaRelTuple
+import co.bondspot.spbttest.domain.entity.Task
 import co.bondspot.spbttest.domain.event.TaskNewEvent
 
-class TaskEventsService : ITaskEventsService {
+class TaskEventsService(private val fga: IFgaProvider, private val fts: IFullTextSearchProvider) :
+    ITaskEventsService {
     override fun handleTaskNewEvent(e: TaskNewEvent) {
-        TODO("Not yet implemented")
+        // TODO if error thrown in this block, remove created task, since no action could be
+        //  done from any account.
+
+        fga.writeRelationship(
+            FgaRelTuple(
+                Account.ENTITY_NAME to e.triggerAccountId,
+                Task.FgaRelations.OWNER,
+                Task.ENTITY_NAME to e.task.id!!,
+            )
+        )
+
+        fts.index(Task.ENTITY_NAME, listOf(e.task))
     }
 }
--- src/test/kotlin/co/bondspot/spbttest/application/service/TaskEventsServiceTests.kt
+++ src/test/kotlin/co/bondspot/spbttest/application/service/TaskEventsServiceTests.kt
@@ -1,31 +1,65 @@
 package co.bondspot.spbttest.application.service
 
+import co.bondspot.spbttest.domain.contract.IFgaProvider
+import co.bondspot.spbttest.domain.contract.IFullTextSearchProvider
+import co.bondspot.spbttest.domain.entity.Account
+import co.bondspot.spbttest.domain.entity.FgaRelTuple
 import co.bondspot.spbttest.domain.entity.Task
 import co.bondspot.spbttest.domain.event.TaskNewEvent
+import io.mockk.spyk
+import io.mockk.verify
 import java.util.UUID
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
 
 class TaskEventsServiceTests {
+    private lateinit var service: TaskEventsService
+    private lateinit var fts: IFullTextSearchProvider
+    private lateinit var fga: IFgaProvider
+
+    @BeforeEach
+    fun setup() {
+        fts = spyk<IFullTextSearchProvider>()
+        fga = spyk<IFgaProvider>()
+
+        service = TaskEventsService(fga, fts)
+    }
+
     @Nested
     @DisplayName("Create new notification for a task")
     inner class CreateTaskNotification {
         @Test
         fun `not implemented`() {
-            val service = TaskEventsService()
+            val accountId = UUID.randomUUID().toString()
+            val task = Task("title", description = "desc", id = UUID.randomUUID().toString())
 
-            // create new notification & notification objects
-            // after identifying the receivers -> owner only or any viewer
-            // is the receivers amount a tradeoff? Like if it was 1M+?
-            // publish send notification (to the receivers) event
+            service.handleTaskNewEvent(TaskNewEvent(task, accountId))
 
-            assertThrows<NotImplementedError> {
-                service.handleTaskNewEvent(
-                    TaskNewEvent(Task(), UUID.randomUUID().toString())
-                )
+            verify(exactly = 1) {
+                fga invoke
+                    "writeRelationship" withArguments
+                    listOf(
+                        FgaRelTuple(
+                            Account.ENTITY_NAME to accountId,
+                            Task.FgaRelations.OWNER,
+                            Task.ENTITY_NAME to task.id!!,
+                        )
+                    )
+            }
+
+            verify(exactly = 1) {
+                fts invoke "index" withArguments listOf(Task.ENTITY_NAME, listOf(task))
             }
         }
     }
+
+    /*
+    TODO: regarding notifications
+     create new notification & notification objects
+     after identifying the receivers -> owner only or any viewer
+     is the receivers amount a tradeoff? Like if it was 1M+?
+     publish send notification (to the receivers) event
+    */
 }
